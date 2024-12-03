ed3e3c47a785e17084a8d843a367c1bbb3210e47
Vinicius Costa <vinicius.costa@urano.pro>
2024-12-03 17:17:05 -0300

    Configuracao inicial

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#011766",
+                secondary: "#007edd",
+                dark: "#0e0e10",
+                gray: {
+                    light: "#e2e2e2",
+                    DEFAULT: "#a5a5a5",
+                },
+                white: {
+                    DEFAULT: "#fbfbfb",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
