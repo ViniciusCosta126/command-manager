4485ef562df2def1239ffc1f8356fcac42e637f4
Vinicius Costa <viniciuscosta126@gmail.com>
2024-12-04 19:17:48 -0300

    Finalizado design inicial

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="sidebar bg-primary h-screen text-white">
+    <h1 class="mt-2 text-center text-2xl font-bold">Gerenciador de comandas</h1>
+
+    <nav class="mt-4">
+        <div class="px-4 flex flex-col">
+            <a href="" class="p-3 hover:bg-secondary rounded ease-in duration-150">
+                <i class="fa-regular fa-credit-card me-2"></i>
+                Comandas
+            </a>
+            <a href="" class="p-3 hover:bg-secondary rounded ease-in duration-150">
+                <i class="fa-regular fa-clipboard me-2"></i>
+                Relatorios
+            </a>
+            <a href="" class="p-3 hover:bg-secondary rounded ease-in duration-150">
+                <i class="fa-solid fa-cart-shopping me-2"></i>
+                Produtos
+            </a>
+            <a href="" class="p-3 hover:bg-secondary rounded ease-in duration-150">
+                <i class="fa-solid fa-layer-group me-2"></i>
+                Categorias
+            </a>
+        </div>
+    </nav>
+</div>
